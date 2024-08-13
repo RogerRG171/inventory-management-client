32e31da610481c6010021bfdf547b826d061dfbc
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-13 15:54:19 -0300

    @feat: Add class to HTML to indicate the current theme mode

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,22 +1,32 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '@/app/(components)/Navbar'
 import Sidebar from '@/app/(components)/Sidebar'
 import StoreProvider, { useAppSelector } from './redux'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  // redux
   const isSideBarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
   )
-
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
+
+  // effects
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('light')
+    }
+  }, [isDarkMode])
+
   return (
     <div
       className={`${isDarkMode ? 'dark' : 'light'} flex bg-gray-50 text-gray-900 w-full min-h-screen`}
     >
       <Sidebar />
       <main
-        className={`${isDarkMode ? 'dark' : 'light'} flex flex-col w-full h-full py-7 p-9 bg-gray-50 ${isSideBarCollapsed ? 'md:pl-24' : 'md:pl-72'}`}
+        className={`flex flex-col w-full h-full py-7 p-9 bg-gray-50 ${isSideBarCollapsed ? 'md:pl-24' : 'md:pl-72'}`}
       >
         <Navbar />
         {children}
