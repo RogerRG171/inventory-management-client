08d13f864c3a600b3c21361703e4ad4b604865a3
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-15 15:16:35 -0300

    @edit: refactoring code

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -13,13 +13,22 @@ const Navbar = () => {
   )
   const dispatch = useAppDispatch()
 
+  // functions
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!darkMode))
+  }
+
+  const toggleSidebar = () => {
+    dispatch(setIsSideBarCollapsed(!isSidebarCollapsed))
+  }
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* Left */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => dispatch(setIsSideBarCollapsed(!isSidebarCollapsed))}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
@@ -27,8 +36,8 @@ const Navbar = () => {
         <div className="relative">
           <input
             type="search"
-            placeholder="Start type to search groups & products"
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+            placeholder="Search groups & products..."
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
           />
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <Bell className="text-gray-500" size={20} />
@@ -39,7 +48,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => dispatch(setIsDarkMode(!darkMode))}>
+            <button onClick={toggleDarkMode}>
               {!darkMode ? (
                 <Moon className="cursor-pointer text-gray-500" size={24} />
               ) : (
