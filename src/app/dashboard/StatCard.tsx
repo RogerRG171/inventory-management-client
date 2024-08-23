a88ed8e4f364def5c7bcc2c28d853d2e27749021
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-23 16:13:22 -0300

    @feat: create stat card

---
+++ src/app/dashboard/StatCard.tsx
@@ -0,0 +1,72 @@
+import { LucideIcon } from 'lucide-react'
+import React from 'react'
+
+type StatDetail = {
+  title: string
+  amount: string
+  changePercentage: number
+  IconComponent: LucideIcon
+}
+type StatCardProps = {
+  title: string
+  primaryIcon: JSX.Element
+  details: StatDetail[]
+  dateRange: string
+}
+
+const StatCard = ({
+  title,
+  primaryIcon,
+  details,
+  dateRange,
+}: StatCardProps) => {
+  const formatPercentage = (value: number) => {
+    const signal = value >= 0 ? '+' : ''
+    return `${signal}${value.toFixed(2)}%`
+  }
+
+  const getChangeColor = (value: number) =>
+    value >= 0 ? 'text-green-500' : 'text-red-500'
+  return (
+    <div className="flex flex-col justify-between md:row-span-1 xl:row-span-2 col-span-1 bg-white shadow-md rounded-2xl pb-3">
+      {/* Header */}
+      <div>
+        <div className="flex justify-between items-center mb-2 px-5 pt-3">
+          <h2 className="text-lg font-semibold text-gray-700">{title}</h2>
+          <span className="text-xs text-gray-400">{dateRange}</span>
+        </div>
+        <hr />
+      </div>
+      {/* Body */}
+      <div className="grid grid-cols-4 grid-rows-2 text-center gap-4 mt-4 md:pr-7 pr-2">
+        <div className="row-span-2 flex items-center justify-center">
+          <div className="relative h-6 w-6 rounded-full p-5  bg-blue-50 border-sky-300 border-[1px] transform translate-x-0">
+            {primaryIcon}
+          </div>
+        </div>
+        {details.map(
+          ({ title, amount, changePercentage, IconComponent }, index) => (
+            <React.Fragment key={title}>
+              <span className="flex items-center justify-center text-gray-500">
+                {title}
+              </span>
+              <span className="flex items-center justify-center font-bold text-gray-800 ">
+                {amount}
+              </span>
+              <span
+                className={`flex items-center justify-center  ${getChangeColor(changePercentage)}`}
+              >
+                <IconComponent
+                  className={`w-4 h-4 mr-2 overflow-visible ${getChangeColor(changePercentage)}`}
+                />
+                {formatPercentage(changePercentage)}
+              </span>
+            </React.Fragment>
+          ),
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default StatCard
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import { Package, TrendingDown, TrendingUp } from 'lucide-react'
 import CardExpenseSummary from './CardExpenseSummary'
 import CardPopularProducts from './CardPopularProducts'
 import CardPurchaseSummary from './CardPurchaseSummary'
 import CardSalesSummary from './CardSalesSummary'
+import StatCard from './StatCard'
 
 const Dashboard = () => {
   return (
@@ -12,10 +14,29 @@ const Dashboard = () => {
       <CardSalesSummary />
       <CardPurchaseSummary />
       <CardExpenseSummary />
-
-      <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
-      <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
-      <div className="md:row-span-3 xl:row-span-2 bg-gray-500"></div>
+      <StatCard
+        title="Customer & Expenses"
+        dateRange="22 - 29 October 2023 "
+        primaryIcon={
+          <Package className="w-6 h-6 text-blue-500 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+        }
+        details={[
+          {
+            title: 'Customers Growth',
+            amount: '175.00',
+            changePercentage: 131,
+            IconComponent: TrendingUp,
+          },
+          {
+            title: 'Expenses',
+            amount: '10.00',
+            changePercentage: -56,
+            IconComponent: TrendingDown,
+          },
+        ]}
+      />
+      <div className="flex flex-col justify-between md:row-span-1 xl:row-span-2 col-span-1  shadow-md rounded-2xl pb-3 bg-gray-500"></div>
+      <div className="flex flex-col justify-between md:row-span-1 xl:row-span-2 col-span-1  shadow-md rounded-2xl pb-3 bg-gray-500"></div>
     </div>
   )
 }
