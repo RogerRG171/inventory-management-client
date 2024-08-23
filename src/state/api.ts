d82342f9fff1a5ff038b58d2d18dd11af3fbc6fc
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-23 17:38:33 -0300

    @feat: create products media query on api

--- src/state/api.ts
+++ src/state/api.ts
@@ -9,6 +9,13 @@ export interface Product {
   rating?: number
 }
 
+export interface NewProduct {
+  name: string
+  price: number
+  stockQuantity: number
+  rating?: number
+}
+
 export interface SalesSummary {
   salesSummaryId: string
   totalValue: number
@@ -46,13 +53,32 @@ export interface DashboardMetrics {
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: 'api',
-  tagTypes: ['DashboardMetrics'],
+  tagTypes: ['DashboardMetrics', 'Products'],
   endpoints: (build) => ({
     getDashBoardMetrics: build.query<DashboardMetrics, void>({
       query: () => '/dashboard',
       providesTags: ['DashboardMetrics'],
     }),
+    getProducts: build.query<Product[], string | void>({
+      query: (search) => ({
+        url: '/products',
+        params: search ? { search } : {},
+      }),
+      providesTags: ['Products'],
+    }),
+    createProduct: build.mutation<Product, NewProduct>({
+      query: (newProduct) => ({
+        url: '/products',
+        method: 'POST',
+        body: newProduct,
+      }),
+      invalidatesTags: ['Products'],
+    }),
   }),
 })
 
-export const { useGetDashBoardMetricsQuery } = api
+export const {
+  useGetDashBoardMetricsQuery,
+  useGetProductsQuery,
+  useCreateProductMutation,
+} = api
