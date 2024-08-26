37fd8ff2a50e7defa4ad70196bdcad8081daade5
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-26 15:47:15 -0300

    @fix: correct flex on data grid

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -15,12 +15,13 @@ const columns: GridColDef[] = [
   {
     field: 'name',
     headerName: 'Product Name',
-    width: 200,
+    width: 150,
   },
   {
     field: 'price',
     headerName: 'Price',
     width: 110,
+
     type: 'number',
     valueGetter: (value, row) => `$${row.price}`,
   },
@@ -61,7 +62,8 @@ const Inventory = () => {
         columns={columns}
         getRowId={(row) => row.productId}
         checkboxSelection
-        className="bg-white shadow-md rounded-lg border border-gray-200 mt-5 !text-gray-700 w-fit h-screen"
+        scrollbarSize={16}
+        className="bg-white shadow-md rounded-lg border border-gray-200 mt-5 !text-gray-700"
         sx={{
           '& .MuiDataGrid-columnHeader, .MuiDataGrid-footerContainer, .MuiDataGrid-row--borderBottom .MuiDataGrid-filler':
             {
