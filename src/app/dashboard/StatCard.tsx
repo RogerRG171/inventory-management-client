2fc4d915c8dc822c31ae0c1e7d09d9e503285480
RogerGN <roger.goulart.nascimento@gmail.com>
2024-08-28 18:49:25 -0300

    @fix: add heigth to the card

--- src/app/dashboard/StatCard.tsx
+++ src/app/dashboard/StatCard.tsx
@@ -28,7 +28,7 @@ const StatCard = ({
   const getChangeColor = (value: number) =>
     value >= 0 ? 'text-green-500' : 'text-red-500'
   return (
-    <div className="flex flex-col justify-between md:row-span-1 xl:row-span-2 col-span-1 bg-white shadow-md rounded-2xl pb-3">
+    <div className="flex flex-col justify-between md:row-span-1 xl:row-span-2 col-span-1 bg-white shadow-md rounded-2xl pb-6">
       {/* Header */}
       <div>
         <div className="flex justify-between items-center mb-2 px-5 pt-3">
@@ -38,7 +38,7 @@ const StatCard = ({
         <hr />
       </div>
       {/* Body */}
-      <div className="grid grid-cols-4 grid-rows-2 text-center gap-4 mt-4 md:pr-7 pr-2">
+      <div className="grid grid-cols-4 grid-rows-2 text-center gap-4 mt-4 md:pr-7 pr-2 h-full">
         <div className="row-span-2 flex items-center justify-center">
           <div className="relative h-6 w-6 rounded-full p-5  bg-blue-50 border-sky-300 border-[1px] transform translate-x-0">
             {primaryIcon}
@@ -50,7 +50,7 @@ const StatCard = ({
               <span className="flex items-center justify-center text-gray-500">
                 {title}
               </span>
-              <span className="flex items-center justify-center font-bold text-gray-800 ">
+              <span className="flex items-center justify-center font-bold text-gray-800">
                 {amount}
               </span>
               <span
